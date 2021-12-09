5d1a5653cb80b7ef4210949db347a6d1a454559c
shyeon <ehftozl235@gmanil.com>
2021-12-09 10:12:37 +0900

    ListController.java modified, RestaurantList folder totalListView.jsp add

--- src/main/java/kh/com/semi_project/controller/ListController.java
+++ src/main/java/kh/com/semi_project/controller/ListController.java
@@ -34,86 +34,88 @@ public class ListController extends HttpServlet {
 		actionDo(request, response);
 	}
 
-	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void actionDo(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
-		
+
 		String url = request.getRequestURI();
 		String ctxPath = request.getContextPath();
 		String cmd = url.substring(ctxPath.length());
 		ListDAO dao = new ListDAO();
-		
+
 		// 리스트 등록(리스트 사진도 같이 등록)
-		if(cmd.equals("/addListProc.li")) {
+		if (cmd.equals("/addListProc.li")) {
 			System.out.println("요청도착");
-			
+
 			String filePath = request.getServletContext().getRealPath("listFiles");
 			System.out.println(filePath);
 			File dir = new File(filePath);
-			if(!dir.exists()) {
+			if (!dir.exists()) {
 				dir.mkdir();
 			}
 			int fileSize = 1024 * 1024 * 10;
-			
+
 			System.out.println("filePath : " + filePath);
 			System.out.println("fileSize : " + fileSize);
 			try {
-				MultipartRequest multi = new MultipartRequest(request, filePath, fileSize, "utf-8", new DefaultFileRenamePolicy());
-				
+				MultipartRequest multi = new MultipartRequest(request, filePath, fileSize, "utf-8",
+						new DefaultFileRenamePolicy());
+
 				String list_title = multi.getParameter("title");
-				
+
 				int seq_list = dao.getListSequence();
 				int rs = dao.addList(new ListDTO(seq_list, list_title));
-				
+
 				String origin_name = multi.getOriginalFileName("listImg");
 				String system_name = multi.getFilesystemName("listImg");
 				System.out.println("origin_name : " + origin_name);
 				System.out.println("system_name : " + system_name);
-				
+
 				ListFileDAO daoLFile = new ListFileDAO();
 				int rsFile = daoLFile.insertFile(new ListFileDTO(0, seq_list, origin_name, system_name));
-				
-				if(rs != -1 && rsFile != -1) {
+
+				if (rs != -1 && rsFile != -1) {
 					response.getWriter().write("success");
 				} else {
 					response.getWriter().write("fail");
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(cmd.equals("/getRestaurantListProc.li")) {  // 리스트 전체 목록 불러오기
+		} else if (cmd.equals("/getRestaurantListProc.li")) { // 리스트 전체 목록 불러오기
 			System.out.println("요청도착");
-			
+
 			try {
 				ArrayList<ListDTO> list = dao.selectAll();
 				Gson gson = new Gson();
 				String rs = gson.toJson(list);
-			
-				if(list != null) {
+
+				if (list != null) {
 					response.getWriter().write(rs);
 				} else {
 					response.getWriter().write("fail");
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(cmd.equals("/toSelectList.li")) { // 리스트 번호로 해당 리스트 정보불러오기
+		} else if (cmd.equals("/toSelectList.li")) { // 리스트 번호로 해당 리스트 정보불러오기
 			System.out.println("요청도착");
-			
+
 			try {
 				int seq_list = Integer.parseInt(request.getParameter("seq_list"));
 				System.out.println(seq_list);
 				ListDTO dto = dao.selectBySeq(seq_list);
-				
+
 				Gson gson = new Gson();
 				String rs = gson.toJson(dto);
-				
-				if(dto != null) {
+
+				if (dto != null) {
 					response.getWriter().write(rs);
 				} else {
 					response.getWriter().write("fail");
 				}
-			
+
 //				ListFileDAO daoFile = new ListFileDAO();
 //				ListFileDTO dtoFile = daoFile.getFile(seq_list);
 //				
@@ -129,31 +131,31 @@ public class ListController extends HttpServlet {
 //				} catch (Exception e) {
 //					e.printStackTrace();
 //				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(cmd.equals("/deleteListProc.li")) { // 리스트와 파일삭제
+		} else if (cmd.equals("/deleteListProc.li")) { // 리스트와 파일삭제
 			System.out.println("요청도착");
-			
+
 			try {
-				
+
 				int seq_list = Integer.parseInt(request.getParameter("seq_list"));
 				ListFileDAO daoFile = new ListFileDAO();
-				
+
 				// 리스트 삭제
 				int rs = dao.deleteBySeq(seq_list);
-				
+
 				// 실제 경로에 있는 파일 삭제
 				ListFileDTO dtoFile = daoFile.getFile(seq_list);
 				String system_name = dtoFile.getSystem_name();
 				String deleteFilePath = request.getServletContext().getRealPath("listFiles");
-				String filePath = deleteFilePath + File.separator + system_name; 
+				String filePath = deleteFilePath + File.separator + system_name;
 				File deleteFile = new File(filePath);
-				
+
 				// 해당 리스트 파일 정보 삭제
 				int rsFile = daoFile.deleteFile(seq_list);
-				
-				if(deleteFile.exists() && rs == 1 && rsFile == 1) {
+
+				if (deleteFile.exists() && rs == 1 && rsFile == 1) {
 					deleteFile.delete();
 					response.getWriter().write("success");
 					System.out.println("파일이 삭제되었습니다.");
@@ -161,79 +163,89 @@ public class ListController extends HttpServlet {
 					response.getWriter().write("fail");
 					System.out.println("파일이 존재하지 않습니다.");
 				}
-				
-			}catch(Exception e) {
+
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(cmd.equals("/modifyListProc.li")) { // 리스트 정보 수정, 해당 리스트 파일정보도 수정
+		} else if (cmd.equals("/modifyListProc.li")) { // 리스트 정보 수정, 해당 리스트 파일정보도 수정
 			System.out.println("요청도착");
-			
+
 			String filePath = request.getServletContext().getRealPath("listFiles");
 			System.out.println(filePath);
 			File dir = new File(filePath);
-			if(!dir.exists()) {
+			if (!dir.exists()) {
 				dir.mkdir();
 			}
 			int fileSize = 1024 * 1024 * 10;
-			
+
 			System.out.println("filePath : " + filePath);
 			System.out.println("fileSize : " + fileSize);
-			
+
 			try {
-				MultipartRequest multi = new MultipartRequest(request, filePath, fileSize, "utf-8", new DefaultFileRenamePolicy());
-				
+				MultipartRequest multi = new MultipartRequest(request, filePath, fileSize, "utf-8",
+						new DefaultFileRenamePolicy());
+
 				int seq_list = Integer.parseInt(multi.getParameter("seq_list"));
 				String title = multi.getParameter("title");
 				File uploadFile = multi.getFile("listImg");
 				System.out.println(seq_list + " : " + title + " : " + uploadFile);
-				
+
 				// 새로 업로드한 파일이 있다면
-				if(uploadFile != null) {
+				if (uploadFile != null) {
 					ListFileDAO daoFile = new ListFileDAO();
 					ListFileDTO dtoFile = daoFile.getFile(seq_list);
-					
+
 					// 기존에 있던 실제 파일 삭제
-					String deleteFilePath = filePath + File.separator + dtoFile.getSystem_name(); 
+					String deleteFilePath = filePath + File.separator + dtoFile.getSystem_name();
 					File deleteFile = new File(deleteFilePath);
-					if(deleteFile.exists()) {
+					if (deleteFile.exists()) {
 						deleteFile.delete();
 						System.out.println("파일이 삭제되었습니다.");
 					} else {
 						System.out.println("파일이 존재하지 않습니다.");
 					}
-					
+
 					// 리스트 정보 수정
 					int rs = dao.modifyBySeq(new ListDTO(seq_list, title));
-					
+
 					// 리스트 파일 정보 수정
 					String origin_name = multi.getOriginalFileName("listImg");
 					String system_name = multi.getFilesystemName("listImg");
 					int rsFile = daoFile.modifyFile(new ListFileDTO(0, seq_list, origin_name, system_name));
-					
-					if(rs == 1 && rsFile == 1) {
+
+					if (rs == 1 && rsFile == 1) {
 						response.getWriter().write("success");
 					} else {
 						response.getWriter().write("fail");
 					}
-					
+
 				} else { // 새로 업로드한 파일이 없다면
 					// 리스트 정보만 수정
 					int rs = dao.modifyBySeq(new ListDTO(seq_list, title));
-					
-					if(rs == 1) {
+
+					if (rs == 1) {
 						response.getWriter().write("success");
 					} else {
 						response.getWriter().write("fail");
 					}
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(cmd.equals("/toListDetail.li")) {
+		} else if (cmd.equals("/toTotalListView.li")) {       // 리스트 전체 목록 조회 페이지로 이동
+			System.out.println("요청도착");
 			
-			int seq_list = Integer.parseInt(request.getParameter("seq_list"));
-			
-			
+			try {
+				ArrayList<ListDTO> list = dao.selectAll();
+
+				if (list != null) {
+					request.setAttribute("list", list);
+					request.getRequestDispatcher("/RestaurantList/totalListView.jsp").forward(request, response);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
 	}
 }
